ae3d8168eb9d37374720506396bec97ab982de37
Klvst3r <klvst3r@gmail.com>
2025-11-18 13:27:11 -0600

    Aumentar la edad del Objeto usuario con los botones reactivos

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <script setup>
-const idDisabled = false; //Posible valores: true | false
+
+  import { ref } from 'vue';
+  //let user = 0;
+
+  const user = ref({
+    name: 'Klvst3r',
+    lastname: 'IO',
+    age: 43
+  });
+
+  function increment(){
+    //user++;
+    //console.log(user);
+
+    //Vamoa a modificar una propiedad del objeto
+    user.value.age++; //Objeto.value.age
+  }
+
+  function decrement(){
+    user.value.age--;
+    //console.log(user);
+  }
 </script>
 
 <template>
-  <p>
-    {{ isDisabled ? "Deshablitado" : "Habilitado" }}
-  </p>
-  <button :disabled="idDisabled">Haz click</button>
+  <button @click="decrement">-</button>
+  <span>{{ user.age }}</span>
+  <button @click="increment">+</button>
+  
 </template>
-
-<style scoped></style>
+ 
+<style scoped>
+  span{
+    padding: 0 10px;
+  }
+</style>
+ 
