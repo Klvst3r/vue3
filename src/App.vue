6a3c4cc9a6877f443f797cb7bb812e85af6160fb
Klvst3r <klvst3r@gmail.com>
2025-11-19 12:58:45 -0600

    9. Enviamos elementos del array desde el formulario

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
 import { ref } from 'vue'
 
-const name = ref('')
+const name = ref('') //utilizamos con constantes por que el objeto no va a cambiar
 const paises = ref([])
 
 function save() {
   // ejemplo: agregar name a paises
-  if (name.value.trim() !== '') {
+  /* if (name.value.trim() !== '') {
     paises.value.push(name.value)
     name.value = '' // limpiar input
-  }
+  } */
+  //Vamosa acceder al aray paises con el metodo push, pasamos el value del objeto
+  paises.value.push(name.value); //agrtegamos un elemento
 }
 </script>
 
@@ -19,7 +21,7 @@ function save() {
     <button>Guardar</button>
   </form>  
 
-  {{ name }}
+  <!-- {{ name }} -->
 
   <p v-if="paises.length">Paises</p>
   <ul>
