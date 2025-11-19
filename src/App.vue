e467368c3dd6f47a074fd6ef2ebfd202d3dea5f9
Klvst3r <klvst3r@gmail.com>
2025-11-19 11:21:30 -0600

    8. Bucles y condicionales

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <script setup>
   
   let users = [
-    'John Doe',
+   /*  'John Doe',
     'Jane Smith',
     'Alice Johnson',
     'Bob Brown',
     'Charlie Black',
-  ]
+  ] */
+//Colección de objetos
+{
+  'id': 1,
+  'name': 'John Doe',
+  },
+  {
+  'id': 2,
+  'name': 'Jane Smith',
+  },
+  {
+  'id': 3,
+  'name': 'Alice Johnson',
+  },
+  {
+  'id': 4,
+  'name': 'Bob Brown',
+  },
+  {
+  'id': 5,
+  'name': 'Charlie Black',
+  }
+];
 </script>
 
 <template>
-    <ul>
-      <li v-for="(user, index) in users">
-        {{ index }} - {{ user }}</li>
+ <ul>
+      <!-- <li v-for="user in users" v-if="user.id > 2">
+
+        {{ user.id }} - {{ user.name }}
+
+      </li> -->
+      <template v-for="user in users">
+        <li v-if="user.id > 2">
+          {{ user.id }} - {{ user.name }}
+        </li>
+
+      </template>
+        
       </ul>
 </template>
 
