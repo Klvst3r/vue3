d2e6e8ca866d2e0a3ece8f2c4a88cf540e822383
Klvst3r <klvst3r@gmail.com>
2025-11-18 21:43:39 -0600

    Directiva v-if y v-else

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,18 @@
 <script setup>
 
-  import { reactive, computed } from 'vue';
-  //let user = 0;
+  import { ref } from 'vue';
 
-  const user = reactive({
-    name: 'Klvst3r',
-    lastname: 'IO',
-    age: 43
-  });
-
-  const estado = computed(() => {
-    return user.age >= 18 ? 'Eres mayor de edad' : 'Eres menor de edad';
-  });
-
-  function increment(){
-    //user++;
-    //console.log(user);
-
-    //Vamoa a modificar una propiedad del objeto
-    user.age++; //Objeto.value.age
-  }
-
-  function decrement(){
-    user.age--;
-    //console.log(user);
-  }
+  const awesome = ref(false);
+  
 </script>
 
 <template>
-  <button @click="decrement">-</button>
-  <span>{{ user.age }}</span>
-  <button @click="increment">+</button>
+  <p v-if="awesome">Vue es increible</p>
 
-  <p>
-    <!-- Mostrar mensaje según mayoría de edad -->
-    {{ estado }}
-  </p>
-  
-  <!-- <p>{{ estado }}</p>
-
-  <p>{{ estado }}</p>
-
-  <p>{{ estado }}</p> -->
+  <p v-else>Oh no, Vue es horrible</p>
 </template>
  
 <style scoped>
-  span{
-    padding: 0 10px;
-  }
+  
 </style>
  
