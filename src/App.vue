742242472c57293d270a4751e42fab8652e625ab
Klvst3r <klvst3r@gmail.com>
2025-11-19 13:04:50 -0600

    9. Enlace con textarea

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,13 @@
 <script setup>
 import { ref } from 'vue'
 
-const name = ref('') //utilizamos con constantes por que el objeto no va a cambiar
-const paises = ref([])
+const name = ref('');
 
-function save() {
-  // ejemplo: agregar name a paises
-  /* if (name.value.trim() !== '') {
-    paises.value.push(name.value)
-    name.value = '' // limpiar input
-  } */
-  //Vamosa acceder al aray paises con el metodo push, pasamos el value del objeto
-  paises.value.push(name.value); //agrtegamos un elemento
-  name.value = '';
-}
 </script>
 
 <template>
-  <form @submit.prevent="save">
-    <input type="text" v-model="name">
-    <button>Guardar</button>
-  </form>  
-
-  <!-- {{ name }} -->
-
-  <p v-if="paises.length">Paises</p>
-  <ul>
-    <li v-for="(pais, index) in paises" :key="index">
-      {{ pais }}
-    </li>
-  </ul>
+ <textarea v-model="name"></textarea>
+ <p>{{ name }}</p>
 </template>
 
 <style scoped></style>
