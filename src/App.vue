eef63958b41be975dc24a5a26cff7d967c02bfbd
Klvst3r <klvst3r@gmail.com>
2025-11-19 13:38:36 -0600

    9. Enlace en entrada en radio button

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <script setup>
 import { ref } from 'vue'
 
-const name = ref('');
+const numero = ref(1);
 
 </script>
 
 <template>
- <textarea v-model="name"></textarea>
- <p>{{ name }}</p>
+  <div>
+    <label>
+      <input v-model="numero" type="radio" value="1">
+      Uno
+    </label>
+  </div>
+  <div>
+    <label>
+      <input v-model="numero" type="radio" value="2">
+      Dos
+    </label>
+  </div>
+  <div>
+    <label>
+      <input v-model="numero" type="radio" value="3">
+      Tres
+    </label>
+  </div>
+
+  {{ numero }}
 </template>
+ 
 
 <style scoped></style>
