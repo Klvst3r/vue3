57d5bb59a01b63c98f7304d37b09740649b3bc73
Klvst3r <klvst3r@gmail.com>
2025-11-25 10:03:10 -0600

    11. Referencia a los porp del objeto entre componentes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
-// 1. Importar el componente que acabas de crear
-import MyComponent from './components/MyComponent.vue'
+// 1. Importar el componente Detail course
+import DetailCourse from './components/DetailCourse.vue'
+
+let course = {
+  title: 'Vue desde cero',
+  professor: 'Klvst3r',
+  price: 10,
+}
 </script>
 
 <template>
-  <h1>Mi Aplicación Vue 3</h1>
-
-  <MyComponent />
-  <br />
-  <MyComponent />
-  <br />
-  <MyComponent />
-  <br />
-  <MyComponent />
-
-  <p>El código de arriba es el componente importado.</p>
+  <DetailCourse course="Vue 3" />
 </template>
-h1 { color: #42b883; }
+<!-- h1 { color: #35495e; } -->
 <style scoped></style>
---
+++ src/components/DetailCourse.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps(['course'])
+
+ console.log(props.course);
+</script>
+
+<template>
+  <h1>
+    {{ course }}
+  </h1>
+</template>
+
+<style scoped></style>
