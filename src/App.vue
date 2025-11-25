3a03a5d452b032ecde7107a5de94d53bb061c391
Klvst3r <klvst3r@gmail.com>
2025-11-25 11:50:56 -0600

    11. Enviar un array de objetos entre componentes

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,27 @@
 // 1. Importar el componente Detail course
 import DetailCourse from './components/DetailCourse.vue'
 
-let course = {
-  title: 'Vue desde cero',
-  professor: 'Klvst3r',
-  price: 10,
-}
+let courses = [
+  {
+    title: 'Vue desde cero',
+    professor: 'Klvst3r',
+    price: 10,
+  },
+  {
+    title: 'Angular desde cero',
+    professor: 'Klvst3r',
+    price: 15,
+  },
+  {
+    title: 'React desde cero',
+    professor: 'Klvst3r',
+    price: 20,
+  },
+]
 </script>
 
 <template>
-  <DetailCourse :course="course" />
+  <DetailCourse v-for="course in courses" :course="course" />
 </template>
 <!-- h1 { color: #35495e; } -->
 <style scoped></style>
--- src/components/DetailCourse.vue
+++ src/components/DetailCourse.vue
@@ -1,13 +1,17 @@
 <script setup>
 const props = defineProps(['course'])
 
- console.log(props.course);
+//console.log(props.course);
 </script>
 
 <template>
   <h1>
-    {{ course }}
+    {{ course.title }}
   </h1>
+  <p>
+    {{ course.professor }}
+  </p>
+  <p>{{ course.price }}</p>
 </template>
 
 <style scoped></style>
