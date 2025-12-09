69f415296e1d4312e65cde86ee82d961084a52dd
Klvst3r <klvst3r@gmail.com>
2025-12-09 09:28:56 -0600

    3.5 Uso de inheritsAttrs

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,9 @@
 <script setup>
-//1. Importamos ref
-import { ref } from 'vue'
-
-// 2. Importar el componente Detail course
-import DetailCourse from './components/DetailCourse.vue'
-
-let courses = [
-  {
-    title: 'Vue desde cero',
-    professor: 'Klvst3r',
-    price: 10,
-  },
-  {
-    title: 'Angular desde cero',
-    professor: 'Klvst3r',
-    price: 15,
-  },
-  {
-    title: 'React desde cero',
-    professor: 'Klvst3r',
-    price: 20,
-  },
-]
-
-//Pasamos el atributo de manera dinamica:
-const count = ref(5)
-
-
+import AlertComponent from '@/components/AlertComponent.vue'
 </script>
 
 <template>
-  <input type="text" v-model.number="count" />
-
- <DetailCourse
-    v-for="course in courses"
-    :key="course.title"
-    :course="course"
-    v-model:count="count" 
-
-  />
+  <AlertComponent class="bg-red-600" />
 </template>
-<!-- h1 { color: #35495e; } -->
+
 <style scoped></style>
---
+++ src/components/AlertComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+//const props = defineProps(['class'])
+defineOptions({
+  inheritAttrs: false,
+})
+</script>
+
+<template>
+  <div>
+    <div v-bind="$attrs">Hola mundo mundial</div>
+  </div>
+</template>
+
+<style scoped></style>
